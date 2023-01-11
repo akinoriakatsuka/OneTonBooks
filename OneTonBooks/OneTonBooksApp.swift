565e68cc8879c1870df8399dd77e54e9b155424f
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-01-11 23:35:48 +0900

    Initial Commit

---
+++ OneTonBooks/OneTonBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneTonBooksApp.swift
+//  OneTonBooks
+//
+//  Created by 赤塚啓紀 on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct OneTonBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
