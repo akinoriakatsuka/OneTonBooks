d47457b3c225db1b6073ad63f7ac376ded1d4244
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-01-26 01:43:08 +0900

    [add] Firebaseの初期化コードを追加

--- OneTonBooks/OneTonBooksApp.swift
+++ OneTonBooks/OneTonBooksApp.swift
@@ -6,9 +6,24 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
+
+let bookUseCase = BookUseCaseImpl(bookRepository: FirestoreBookRepository())
 
 @main
 struct OneTonBooksApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
